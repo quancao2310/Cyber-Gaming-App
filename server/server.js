ed02f1777c5b51e379e2e489f8c6459b6d03965a
Quan Cao <quancao2310@gmail.com>
2023-12-03 16:43:53 +0700

    initial setup

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json()); /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is deprecated */
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/tutorial.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});

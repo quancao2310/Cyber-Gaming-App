3aa8656a787fcf3437844a3045f4d8ba243c210d
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2023-12-11 14:44:52 +0700

    Add bootstrap 5 for project

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 

9eb1dd9f78e6e37db3a2c5a829acb2a8867bfc04
Huỳnh Nguyên Phúc <phucthcsmyloc@gmail.com>
2023-12-12 22:08:13 +0700

    Add UI for Transaction, Product and Discount Event

---
+++ client/src/components/DiscountEventManager/EventTable.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { format } from 'date-fns';
+import UpdateEventForm from "./UpdateForm";
+
+function EventTable({ data, trigger }) {
+
+  const formatDate = (isoDateTime) => {
+    const date = new Date(isoDateTime);
+    
+    const formattedDateTime = format(date, "MMMM dd, yyyy, HH:mm");
+  
+    return formattedDateTime;
+  }
+
+  const [fetchTrigger, setFetchTrigger] = trigger;
+
+  const handleDelete = async (eventId) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_SERVER_URL}/event/delete/${eventId}`);
+      console.log('Event deleted successfully');
+      setFetchTrigger(!fetchTrigger);
+    } catch (error) {
+      console.error('Error deleting event:', error);
+    }
+  };
+
+  return (
+    <table className="table mt-4">
+      <thead>
+        <tr className="row">
+          <th className="text-center col" scope="col">ID</th>
+          <th className="text-center col" scope="col">Tên</th>
+          <th className="text-center col" scope="col">Ngày bắt đầu</th>
+          <th className="text-center col" scope="col">Ngày kết thúc</th>
+          <th className="text-center col" scope="col">Mức giảm giá</th>
+          <th className="text-center col" scope="col">Hành động</th>
+        </tr>
+      </thead>
+      <tbody id="file-status">
+        {data.map((item, index) => (
+          <tr className="row" key={index}>
+            <td className="text-center col">{item.id}</td>
+            <td className="text-center col">{item.name}</td>
+            <td className="text-center col">{formatDate(item.start_date)}</td>
+            
+            <td className="text-center col">{formatDate(item.end_date)}</td>
+            <td className="text-center col">{item.discount_percent}</td>
+            <td className="text-center col">
+              <UpdateEventForm eventId={item.id} trigger={trigger} />
+              <button
+                className="btn btn-danger m-1"
+                onClick={() => handleDelete(item.id)}
+              >
+                Xóa
+              </button>                
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default EventTable;
---
+++ client/src/components/DiscountEventManager/UpdateForm.js
@@ -0,0 +1,129 @@
+import { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+function UpdateEventForm({ eventId, trigger }) {
+  const [show, setShow] = useState(false);
+  const [fetchTrigger, setFetchTrigger] = trigger;
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  const [formData, setFormData] = useState({
+    name: '',
+    start_date: '',
+    end_date: '',
+    discount_percent: '',
+  });
+
+  const convertToDatetimeLocalFormat = (isoDateTime) => {
+    return isoDateTime ? isoDateTime.slice(0, -1) : '';
+  }
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/event/${eventId}`);
+        const eventDetails = response.data;
+
+        eventDetails.start_date = convertToDatetimeLocalFormat(eventDetails.start_date);
+        eventDetails.end_date = convertToDatetimeLocalFormat(eventDetails.end_date);
+
+        setFormData(eventDetails);
+      } catch (error) {
+        console.error('Error fetching event data:', error);
+      }
+    };
+
+    fetchData();
+  }, [eventId]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const updatedFormData = {
+        ...formData,
+        start_date: formData.start_date + 'Z',
+        end_date: formData.end_date + 'Z',
+      };
+
+      const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/event/update/${eventId}`, updatedFormData);
+      console.log('Event updated successfully:', response.data);
+      setFetchTrigger(!fetchTrigger);
+      handleClose();
+    } catch (error) {
+      console.error('Error updating Event:', error);
+    }
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Cập nhật
+      </Button>
+
+      <Modal 
+        className="text-secondary" 
+        show={show} 
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        onHide={handleClose}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit} className="container mt-4">
+            <div className="mb-3">
+              <label className="form-label">Tên</label>
+              <input type="text" className="form-control" name="name" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Ngày bắt đầu</label>
+              <input
+                type="datetime-local"
+                className="form-control"
+                name="start_date"
+                value={formData.start_date}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Ngày kết thúc</label>
+              <input
+                type="datetime-local"
+                className="form-control"
+                name="end_date"
+                value={formData.end_date}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Mức giảm giá</label>
+              <input type="text" className="form-control" name="discount_percent" value={formData.discount_percent} onChange={handleInputChange} />
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Update Event
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default UpdateEventForm;

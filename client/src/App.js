8716039e6694cadbf329beca6adc2089fbc81b67
CuongDoDuy <cuongthtb08022004@gmail.com>
2023-12-13 14:15:40 +0700

    resolve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AdminHome from "./pages/admin/Home";
 import StaffHome from "./pages/staff/Home";
-import CustomerHome from "./pages/customer/Home"; 
-import BillingProcess from "./pages/staff/BillingProcess"
+import CustomerHome from "./pages/customer/Home";
+import BillingProcess from "./pages/staff/BillingProcess";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import DepositPage from "./components/Cash";
@@ -24,21 +24,27 @@ function App() {
     <div className="app">
       <main className="content">
         <Routes>
-          <Route path="/customer/order/success" element={<OrderSuccessPage/>} />
-          <Route path="/customer/cart" element={<CartPage/>} />
-          <Route path='/customer/order-slot' element={<ComputerOrderPage />} />
-          <Route path='/customer/order-room' element={<OrderRoomPage />} />
-          <Route path='/customer/order-food' element={<FoodOrderPage />} />
+          <Route
+            path="/customer/order/success"
+            element={<OrderSuccessPage />}
+          />
+          <Route path="/customer/cart" element={<CartPage />} />
+          <Route path="/customer/order-slot" element={<ComputerOrderPage />} />
+          <Route path="/customer/order-room" element={<OrderRoomPage />} />
+          <Route path="/customer/order-food" element={<FoodOrderPage />} />
           <Route path="/customer/login" element={<LoginForm />} />
-          <Route path="/customer/cash/success" element={<DepositSuccessPage />} />
+          <Route
+            path="/customer/cash/success"
+            element={<DepositSuccessPage />}
+          />
           <Route path="/customer/cash" element={<DepositPage />} />
           <Route path="/customer" element={<HomeCustomerPage />} />
 
           <Route path="/" element={<Home />} />
-              <Route path="/admin" element={<AdminHome />} />
-              <Route path="/staff" element={<StaffHome />} />
-              <Route path="/customer" element={<CustomerHome />} />
-              <Route path="/billing-process" element={<BillingProcess />} />
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/staff" element={<StaffHome />} />
+          <Route path="/customer" element={<CustomerHome />} />
+          <Route path="/billing-process" element={<BillingProcess />} />
         </Routes>
       </main>
     </div>

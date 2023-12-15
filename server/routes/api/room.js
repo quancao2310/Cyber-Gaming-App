cad1346873306fb21ddd362f9485ead11d0d6af2
CuongDoDuy <cuongthtb08022004@gmail.com>
2023-12-15 08:00:18 +0700

    resolve conflict

--- server/routes/api/room.js
+++ server/routes/api/room.js
@@ -2,9 +2,9 @@ import express from 'express';
 import roomController from '../../controllers/room.controller';
 const roomRoutes = express.Router();
 
-roomRoutes.get('/private-room', roomController.showAllPrivateRoom);
-roomRoutes.get('/public-room', roomController.showAllPublicRoom);
-roomRoutes.get('/:room_type/:room_order', RoomController.show);
+// roomRoutes.get('/private-room', roomController.showAllPrivateRoom);
+// roomRoutes.get('/public-room', roomController.showAllPublicRoom);
+roomRoutes.get('/:room_type/:room_order', roomController.show);
 roomRoutes.get('/', roomController.showAll);
 
 
